015d660fd8505da4d0b8f81cc580314688b7e447
Bogdan Ripa <bogdanripa@gmail.com>
2024-08-23 12:08:45 +0300

    Using the Auth IAC

--- example-postgres/client/src/authProvider.ts
+++ example-postgres/client/src/authProvider.ts
@@ -1,6 +1,6 @@
 import { AuthProvider } from "@refinedev/core";
 import { AuthService } from "@genezio/auth";
-AuthService.getInstance().setTokenAndRegion("1-90fcbc65-eb01-4950-b629-d96423173ca0", "eu-central-1");
+AuthService.getInstance().setTokenAndRegion(import.meta.env.AUTHENTICATION_TOKEN, import.meta.env.AUTHENTICATION_REGION);
 
 export const authProvider: AuthProvider = {
 
